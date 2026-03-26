ccb236d7195aa7e31ab335e89e5b039e1d894389
SSB <132986138+Samvab@users.noreply.github.com>
2026-03-26 23:24:39 +0530

    Update Laptop Price Prediction.py

--- Laptop Price Prediction.py
+++ Laptop Price Prediction.py
@@ -9,7 +9,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import mean_squared_error, r2_score
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as  plt
 import seaborn as sns
 import re
 import base64
