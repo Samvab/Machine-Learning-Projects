4606d186a007c721ac1488b47008563a30658be1
SSB <132986138+Samvab@users.noreply.github.com>
2026-03-29 22:03:00 +0530

    Update Laptop Price Prediction.py

--- Laptop Price Prediction.py
+++ Laptop Price Prediction.py
@@ -1,6 +1,6 @@
      
 import streamlit as st  
-import pandas as pd
+import pandas as pd 
 import numpy as np
 from sklearn.model_selection import train_test_split 
 from sklearn.ensemble import RandomForestRegressor
