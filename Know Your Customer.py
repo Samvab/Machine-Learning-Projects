af36017db3e51527e7c60c8fe3fba57b0170a1d6
SSB <132986138+Samvab@users.noreply.github.com>
2026-03-31 22:33:40 +0530

    Update Know Your Customer.py

--- Know Your Customer.py
+++ Know Your Customer.py
@@ -1,5 +1,5 @@
                                          
-import pandas as pd           
+import pandas as pd            
 from sklearn.model_selection  import train_test_split  
 df = pd.read_csv('/content/Churn_Modelling.csv') 
  
